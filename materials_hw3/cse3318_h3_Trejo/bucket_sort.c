14822a2669bd137af5a74f5eb753a3eae3ef7974
Miguel <mt353469@gmail.com>
2023-03-05 19:55:41 -0600

    file read and stored

--- materials_hw3/cse3318_h3_Trejo/bucket_sort.c
+++ materials_hw3/cse3318_h3_Trejo/bucket_sort.c
@@ -54,12 +54,44 @@ void run1(){
 	// Read a file from the User
 	// open the file 
 	// read the file to get N (size of array)
-
-	// create a 1d array of size N
-	int *pArr[N]; 
-	for(int x=0; x<=N; x++){
-		pArr[x] = NULL; 
+	
+	printf("\n\n Enter a filename: "); 
+	char fileName[100]; 
+	scanf("%s", fileName); 
+	
+	FILE *fp = fopen(fileName, "r");
+	if(fp == NULL){
+		printf("\n Unable to open file \n");
+		return; 
 	}
+	
+	// making a array to hold the scanned value, then copying over to N
+	int arrayN[1]; 
+	fscanf(fp,"%d", &arrayN[0]);
+	
+	int N = arrayN[0]; 
+	
+	
+	// create a 1d array of size N
+	int *pArr = malloc(N * sizeof(int));
+	
+	for(int x=0; x<N; x++){
+		fscanf(fp,"%d", &pArr[x]);
+	}
+	
+	int min = pArr[0];
+	int max = pArr[0];
+	
+	//Finding the min and max
+	for(int x=0; x<N; x++){
+		if(min > pArr[x]){
+			min = pArr[x];
+		}
+		if(max < pArr[x]){
+			max = pArr[x];
+		}
+	}
+	
 	// put the elements from file into the array 
 	// print the array 
 	
@@ -67,6 +99,9 @@ void run1(){
 		//This in itself will have other steps and function calls
 	// print array again (should be sorted now)
 	printf("\n-------run1 - this is a place holder. You need to edit this function.------ \n");
+	
+	fclose(fp);
+	free(pArr);
 }
 
 int main()
