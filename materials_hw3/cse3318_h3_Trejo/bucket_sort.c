00e1280f4e3b001e91009c7678b6441e60985323
Miguel <mt353469@gmail.com>
2023-03-05 22:09:44 -0600

    insert_sorted added

--- materials_hw3/cse3318_h3_Trejo/bucket_sort.c
+++ materials_hw3/cse3318_h3_Trejo/bucket_sort.c
@@ -26,14 +26,58 @@ run without Valgrind:
 void print_array(int arr[], int N);
 void run1();
 
+// my functions 
+void returnFloor(int *arr, int min, int max, int N){
+
+	int floor; 
+	//Not sure if ima keep this
+	int *tempArr = malloc(N * sizeof(int));
+	
+	printf("\n\n Printing the BucketArray \n\n");
+
+	for(int x=0; x<N; x++){
+		
+		floor = (arr[x] - min) * N; 
+		floor = floor / (1 + max - min); 
+		printf("index:%d  value:%d  floor:%d \n",x, arr[x], floor);
+		
+	} 
+	// free (tempArr) along with any other memory allocations (nodes)
+}
+
+
+nodePT insert_sorted(nodePT L, int elem){
+	
+	nodePT new = new_node(elem); 
+	nodePT current = L; 
+	nodePT prev = NULL; 
+
+	if(L == NULL || L->data > elem){
+		new->next = L;
+		L = new;
+	}	
+	else{
+		while(current != NULL && current->data < elem){
+			prev = current; 
+			current = current->next; 
+		}
+		new->next = current; 
+		prev->next = new;
+	}
+	
+	return L;
+}
+
+
 /* 
 // function to insert a new node in a sorted list. E.g.: 
 nodePT insert_sorted(nodePT L, nodePT newP);
 //or
 nodePT insert_sorted(nodePT L, int elem);
-//  function to sort an array sing bucket sort. E.g.:
+//  function to sort an array using bucket sort. E.g.:
 void bucket_sort(int * arr, int N);
 */
+
 
 void print_array(int arr[], int N){
 	int j;
@@ -50,11 +94,8 @@ void run1(){
 	// Fill in your code here. You should not write everything in this function.  
 	// Write some helper functions to separate the work.	
 	
-	//Steps from OneNote
-	// Read a file from the User
-	// open the file 
-	// read the file to get N (size of array)
 	
+	// Getting Filename from user and opening the file
 	printf("\n\n Enter a filename: "); 
 	char fileName[100]; 
 	scanf("%s", fileName); 
@@ -75,9 +116,13 @@ void run1(){
 	// create a 1d array of size N
 	int *pArr = malloc(N * sizeof(int));
 	
+	// Inserting ints from file to pArr and printing
 	for(int x=0; x<N; x++){
 		fscanf(fp,"%d", &pArr[x]);
 	}
+	
+	print_array(pArr, N);
+	
 	
 	int min = pArr[0];
 	int max = pArr[0];
@@ -92,8 +137,7 @@ void run1(){
 		}
 	}
 	
-	// put the elements from file into the array 
-	// print the array 
+
 	
 	// call bucket_sort
 		//This in itself will have other steps and function calls
