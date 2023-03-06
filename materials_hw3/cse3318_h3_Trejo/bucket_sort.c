76ed0e0aec530ae730fe18990fa87375a26ed078
Miguel <mt353469@gmail.com>
2023-03-05 22:33:43 -0600

    working onbucket

--- materials_hw3/cse3318_h3_Trejo/bucket_sort.c
+++ materials_hw3/cse3318_h3_Trejo/bucket_sort.c
@@ -27,22 +27,17 @@ void print_array(int arr[], int N);
 void run1();
 
 // my functions 
-void returnFloor(int *arr, int min, int max, int N){
+int returnFloor(int arrIdx, int min, int max, int N){
 
 	int floor; 
-	//Not sure if ima keep this
-	int *tempArr = malloc(N * sizeof(int));
 	
 	printf("\n\n Printing the BucketArray \n\n");
-
-	for(int x=0; x<N; x++){
 		
-		floor = (arr[x] - min) * N; 
-		floor = floor / (1 + max - min); 
-		printf("index:%d  value:%d  floor:%d \n",x, arr[x], floor);
-		
-	} 
-	// free (tempArr) along with any other memory allocations (nodes)
+	floor = (arrIdx - min) * N; 
+	floor = floor / (1 + max - min); 
+	
+	return floor; 
+	// delete printf progress checks 
 }
 
 
@@ -66,6 +61,33 @@ nodePT insert_sorted(nodePT L, int elem){
 	}
 	
 	return L;
+}
+
+// 
+void bucket_sort(int *arr, int N){
+/*	
+	nodePT B[N+1]; 
+	for(int x=0; x<N; x++){
+		B[x] = NULL;
+	} */
+	
+	int min = arr[0];
+	int max = arr[0];
+	
+	//Finding the min and max
+	for(int x=0; x<N; x++){
+		if(min > arr[x]){
+			min = arr[x];
+		}
+		if(max < arr[x]){
+			max = arr[x];
+		}
+	}
+	
+	printf("\n\n\n HERERERE min:%d max:%d", min, max);
+	
+	
+	
 }
 
 
@@ -124,18 +146,9 @@ void run1(){
 	print_array(pArr, N);
 	
 	
-	int min = pArr[0];
-	int max = pArr[0];
+
 	
-	//Finding the min and max
-	for(int x=0; x<N; x++){
-		if(min > pArr[x]){
-			min = pArr[x];
-		}
-		if(max < pArr[x]){
-			max = pArr[x];
-		}
-	}
+	bucket_sort(pArr, N);
 	
 
 	
