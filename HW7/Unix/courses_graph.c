668dc36bcdb4ddb25f99e40bfe33295ad371424d
Miguel <mt353469@gmail.com>
2023-04-29 13:21:44 -0500

    need to dynamically allocate String

--- HW7/Unix/courses_graph.c
+++ HW7/Unix/courses_graph.c
@@ -18,6 +18,32 @@ int search(char **courseList, char *string){
 	return index; 
 }
 
+
+int **createTable(int rows, int colums){
+	
+	int **array = NULL; 
+
+	array = (int**) malloc(rows*(sizeof(int*)));
+	for(int x = 0; x < rows; x++){
+		array[x] = (int*) malloc(colums*(sizeof(int)));
+		
+		for(int j = 0; j < colums; j++){
+		array[x][j] = 0; 
+		}
+	}
+	
+	return (array);
+}
+
+void freeTable(int **table, int rows){
+	for(int x = 0; x < rows; x++){
+		free(table[x]);
+	}
+	free(table); 
+}
+
+
+// restarting due to segmentation faults
 int main(){
 	
 	int maxStr = 30;
@@ -31,13 +57,11 @@ int main(){
 	
 	char *token;
 	
-	// token? 
-	
+	//Recieving fileName
 	printf("\n Enter file name:\n"); 
 	fgets(filename, maxStr, stdin);
-	
 	removeN(filename);
-	
+	//Opening File
 	FILE *fp = fopen(filename, "r"); 
 	
 	if(fp == NULL){
@@ -45,136 +69,22 @@ int main(){
 		return EXIT_FAILURE;
 	}
 	
-	
-	
-	/*	File is opening and repeating last value
-	*/
-	
-	//I mean not what i expected but it works for the first 
-	//parameters
-	// can repeat loop to find out # of lines == size of array
-	// gets size of file for array 
+	// Gets lenght for table
 	while ( fgets(fileLine, maxLine, fp) != NULL ){
 		token = strtok(fileLine, " ");
 		printf("\n %d string: %s \n ",lines, token); 
 		lines++;
 	}
-	// Dynamically allocate it later? can do lenght too [x][x]
-	char *courses[lines];
-	int table[lines][lines]; //
-	//resetting file pointer 
-	fseek(fp, 0, SEEK_SET);
-	 lines = 0;
-	 
-	 	printf("\n PRIIINT = %s ", token);
 	
-	//stores values into string array 
-	while ( fgets(fileLine, maxLine, fp) != NULL ){
-		
-		 // put into a loop?
-		token = strtok(fileLine, " ");
-		strcpy(courses[lines], token);
-		printf("\n courses[%d]: %s ",lines, courses[lines]);
-		printf("\n %d string: %s \n ",lines, token); 
-		lines++;
-		
-	}
-	printf("\n test \n");
-	fseek(fp, 0, SEEK_SET);
-	printf("\n token BEFORE WHILE = %s ", token);
-	int colum; 
-	int row; 
-	
-	
-	// maybe set int arr all = 0 first ?
-	
-	
-	while ( fgets(fileLine, maxLine, fp) != NULL ){
-		//gets first token for [][y]
-		printf("\n Entered While loop ");
-		token = strtok(fileLine, " ");
-		printf("\n token = %s ", token);
-		colum = search(courses, token); 
-		printf("\n Colum:%d ", colum); 
-		
-		/*if( (token = strtok(NULL, " ")) != NULL ){ 
-			row = search(courses, token); 
-			printf("\n row:%d", row);
-			
-		}*/
-		
-		//for x=0; token != Null; x++ 
-			//token = strtok(Null, )
-			// search() = x, arr[x][y] = 1
-		
-	} 
-/*	
-	// Benefit of this for loop, is recieving the 1st token
-	for(int x=0; x<lines; x++){ // this is suppose to read amount of lines in file
-		fgets(fileLine, maxLine, fp); 
-		//this will get the first line in the file 
-		
-		token = strtok(fileLine, " ");
-		
-		/*So i have the first line, ex c300
-		//using search(), will return the index location (which can be 
-		//used interchangably with x or y, ), we will use the return int 
-		as the [][y] the colum value 
-		//
-	}
-	*/
-	
-	/* needs work 
-	for(int x=0; x<lines; x++){
-		//token1
-		fgets(fileLine, maxLine, fp); 
-		token = strtok(fileLine, " ");  // need to test tokenDeilim
-		colum = search(courses, token); 
-		printf("\n test string %s  \n", token);
-		printf("\n test2 \n");
-		while(token != NULL){
-			token = strtok(NULL, " ");
-			printf("\n token:%s ", token);
-			if (token != NULL){
-				row = search(courses, token); 
-				table[row][colum] = 1; 
-				printf("\n %d:%s --> %d:%s",row, courses[row], colum, courses[colum]);
-				}
-		}
-		
-	} 
-*/	
-	
-	/*
-		restart fp 
-		
-		need to learn how to get a line (fgets), tokenize it
-		and be able to use all values in that table, 
-		[token1, 2, 3]
-		{
-		token1==y will be colum arr[][y]
-		insert token1 into search() to get y 
-		
-		after the first token, insert into a loop, until '\n'
-			inside this loop token2, ... into search()
-			this int return will be our x, arr[x][y] = 1 
-		
-		
-		
-		
-		
-			
-	
-	*/
-	
-	
-	/*
-		Using enum? 
-	*/
+	//Dynamically allocate int and string
+	int **table = createTable(lines, lines); 
 	
 	
 	
 	
+	
+	free(fp); 
+	freeTable(table, lines);
 	
 	return EXIT_SUCCESS; 
 }
