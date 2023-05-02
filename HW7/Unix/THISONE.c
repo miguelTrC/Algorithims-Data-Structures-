67fdcfaf769fc231ec6f9be328baa00c231c50e7
Miguel <mt353469@gmail.com>
2023-05-02 18:34:17 -0500

    TURN THIS ONE in

--- HW7/Unix/THISONE.c
+++ HW7/Unix/THISONE.c
@@ -1,101 +1,62 @@
+//Miguel Trejo 1001685532
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Course {
-    char *name;
-    struct Course **prerequisites;
-    int prereq_count;
-} Course;
 
-enum Color { WHITE, GRAY, BLACK };
-
-void DFS(Course *courses, int course_count);
-void DFS_visit(Course *courses, int course_count, int u, enum Color *color, int *pred, int *finish, int *time);
-
-int main() {
-    char filename[100];
-    printf("Enter the filename: ");
-    scanf("%s", filename);
-
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Error opening the file.\n");
-        return 1;
-    }
-
-    Course *courses = NULL;
-    int course_count = 0;
-    char line[1000];
-
-    while (fgets(line, sizeof(line), file)) {
-        char *course_name = strtok(line, " \n");
-        Course *course = malloc(sizeof(Course));
-        course->name = strdup(course_name);
-        course->prerequisites = NULL;
-        course->prereq_count = 0;
-
-        course_count++;
-        courses = realloc(courses, course_count * sizeof(Course));
-        courses[course_count - 1] = *course;
-
-        char *prereq_name;
-        while ((prereq_name = strtok(NULL, " \n")) != NULL) {
-            course->prereq_count++;
-            course->prerequisites = realloc(course->prerequisites, course->prereq_count * sizeof(Course *));
-            course->prerequisites[course->prereq_count - 1] = malloc(sizeof(Course));
-            course->prerequisites[course->prereq_count - 1]->name = strdup(prereq_name);
-        }
-    }
-
-    fclose(file);
-
-    DFS(courses, course_count);
-
-    for (int i = 0; i < course_count; i++) {
-        free(courses[i].name);
-        for (int j = 0; j < courses[i].prereq_count; j++) {
-            free(courses[i].prerequisites[j]);
-        }
-        free(courses[i].prerequisites);
-    }
-    free(courses);
-
-    return 0;
+// removes '\n'
+void removeN (char *string){
+	string[strcspn(string, "\n")] = 0;
 }
 
-void DFS(Course *courses, int course_count) {
-    enum Color *color = malloc(course_count * sizeof(enum Color));
-    int *pred = malloc(course_count * sizeof(int));
-    int *finish = malloc(course_count * sizeof(int));
-    int time = 0;
+// Struct that will hold beggining course and its pre-requisites
+typedef struct Course {
+    char *name[30];
+    struct Course **prerequisites;
+    int prereqCount;
+} Course;
 
-    for (int u = 0; u < course_count; u++) {
+//Will be used in DFS
+enum Color { WHITE, GRAY, BLACK };
+
+
+
+void DFS(Course *courses, int courseCount) {
+    enum Color *color = malloc(courseCount * sizeof(enum Color));
+    int *pred = malloc(courseCount * sizeof(int));
+    int *finish = malloc(courseCount * sizeof(int));
+    int visited = 0; // 0=White, 1=Grey, 2=black
+
+
+	//unvisited vertices
+    for (int u = 0; u < courseCount; u++) {
         color[u] = WHITE;
         pred[u] = -1;
     }
 
-    for (int u = 0; u < course_count; u++) {
+	//visiting vertices
+    for (int u = 0; u < courseCount; u++) {
         if (color[u] == WHITE) {
-            DFS_visit(courses, course_count, u, color, pred, finish, &time);
+            DFS_visit(courses, courseCount, u, color, pred, finish, &visited);
         }
     }
 
-    printf("Topological Sort:\n");
-    for (int i = course_count - 1; i >= 0; i--) {
-        int index = -1;
-        int max_finish = -1;
+    printf("\nTopological Sort:\n");
+    for (int i = courseCount - 1; i >= 0; i--) {
+        int idx = -1;
+        int maxFinish = -1;
 
-        for (int j = 0; j < course_count; j++) {
-            if (finish[j] > max_finish) {
-                max_finish = finish[j];
-                index = j;
+        for (int j = 0; j < courseCount; j++) {
+            if (finish[j] > maxFinish) {
+                maxFinish = finish[j];
+                idx = j;
             }
         }
 
-        if (index != -1) {
-            printf("%s\n", courses[index].name);
-            finish[index] = -1;
+        if (idx != -1) {
+            printf("%s\n", courses[idx].name);
+            finish[idx] = -1;
        
         }
     }
@@ -105,26 +66,105 @@ void DFS(Course *courses, int course_count) {
     free(finish);
 }
 
-void DFS_visit(Course *courses, int course_count, int u, enum Color *color, int *pred, int *finish, int *time) {
+void DFS_visit(Course *courses, int courseCount, int u, enum Color *color, int *pred, int *finish, int *visited) {
+	// Vertix is visited
     color[u] = GRAY;
-    (*time)++;
+    (*visited)++;
     
-    for (int i = 0; i < courses[u].prereq_count; i++) {
+    // going thru the pre-req courses
+    for (int i = 0; i < courses[u].prereqCount; i++) {
         int y = -1;
         
-        for (int j = 0; j < course_count; j++) {
+        // Finding vertices of that lead to current
+        for (int j = 0; j < courseCount; j++) {
             if (strcmp(courses[u].prerequisites[i]->name, courses[j].name) == 0) {
                 y = j;
                 break;
             }
         }
-
+		
+		// Performing DFS on the pre-req
         if (y != -1 && color[y] == WHITE) {
             pred[y] = u;
-            DFS_visit(courses, course_count, y, color, pred, finish, time);
+            DFS_visit(courses, courseCount, y, color, pred, finish, visited);
         }
     }
     
     color[u] = BLACK;
-    finish[u] = ++(*time);
+    finish[u] = ++(*visited);
 }
+
+
+
+
+int main() {
+
+	int maxStr = 30;
+	int maxLine = 1000;
+	
+    char filename[maxStr];
+    char fileLine[maxLine];
+
+	
+	//Recieving fileName
+	printf("\n Enter file name:\n"); 
+	fgets(filename, maxStr, stdin);
+	removeN(filename);
+	
+	//Opening File
+	FILE *fp = fopen(filename, "r"); 
+	
+	if(fp == NULL){
+		printf("\n Unable to open file\n"); 
+		return EXIT_FAILURE;
+	}
+	
+
+    Course *courses = NULL;
+    int courseCount = 0;
+    
+	//File reading
+    while (fgets(fileLine, maxStr, fp)) {
+        char *courseName = strtok(fileLine, " \n");
+        Course *course = malloc(sizeof(Course));
+        
+        // first token storing
+        printf("\n Course: %s ", courseName);
+        strcpy(course->name,courseName);
+        course->prerequisites = NULL;
+        course->prereqCount = 0;
+
+		//Traking amount of vertices
+        courseCount++;
+        courses = realloc(courses, courseCount * sizeof(Course));
+        courses[courseCount - 1] = *course;
+
+		// Assigning pre-reqs(if any)
+        char *prereqName;
+        while ((prereqName = strtok(NULL, " \n")) != NULL) {
+            course->prereqCount++;
+            course->prerequisites = realloc(course->prerequisites, course->prereqCount * sizeof(Course *));
+            course->prerequisites[course->prereqCount - 1] = malloc(sizeof(Course));
+            strcpy(course->prerequisites[course->prereqCount - 1]->name,prereqName);
+        }
+    }
+
+    fclose(fp);
+
+    DFS(courses, courseCount);
+
+
+
+    for (int x = 0; x < courseCount; x++) {
+        free(courses[x].name);
+        for (int z = 0; z < courses[x].prereqCount; z++) {
+            free(courses[x].prerequisites[x]);
+        }
+        free(courses[x].prerequisites);
+    }
+    free(courses);
+
+    return EXIT_SUCCESS;
+}
+
+
