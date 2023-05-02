0cf9f8a51537fa2dfc73dfac1d907a9bf3fa84a8
miguelTrC <111913143+miguelTrC@users.noreply.github.com>
2023-05-02 15:27:57 -0500

    Add files via upload

--- HW7/Unix/TryThis.c
+++ HW7/Unix/TryThis.c
@@ -12,30 +12,30 @@ struct Course {
     char color[6];
 };
 
-void DFS_visit(Course *G[], int u, char *color[], Course *pred[], Course *black[], int *num_black) {
-    color[u] = "gray";
+void DFS_visit(Course *G[], int u, Course *color[], Course *pred[], Course *black[], int *num_black) {
+    strcpy(color[u]->color, "gray");
     for (int i = 0; i < G[u]->num_prereqs; i++) {
         Course *v = G[u]->prereqs[i];
-        if (strcmp(color[v->name], "white") == 0) {
+        if (strcmp(v->color, "white") == 0) {
             pred[v->name] = G[u];
             DFS_visit(G, v->name, color, pred, black, num_black);
-        } else if (strcmp(color[v->name], "gray") == 0) {
+        } else if (strcmp(v->color, "gray") == 0) {
             printf("Cycle detected between courses %s and %s\n", G[u]->name, v->name);
         }
     }
-    color[u] = "black";
+    strcpy(color[u]->color, "black");
     black[*num_black] = G[u];
     (*num_black)++;
 }
 
-void DFS(Course *G[], int n, char *color[], Course *pred[], Course *black[], int *num_black) {
+void DFS(Course *G[], int n, Course *color[], Course *pred[], Course *black[], int *num_black) {
     for (int i = 0; i < n; i++) {
-        color[G[i]->name] = "white";
-        pred[G[i]->name] = NULL;
+        strcpy(G[i]->color, "white");
+        pred[i] = NULL;
     }
     for (int i = 0; i < n; i++) {
-        if (strcmp(color[G[i]->name], "white") == 0) {
-            DFS_visit(G, G[i]->name, color, pred, black, num_black);
+        if (strcmp(G[i]->color, "white") == 0) {
+            DFS_visit(G, i, color, pred, black, num_black);
         }
     }
 }
@@ -43,7 +43,7 @@ void DFS(Course *G[], int n, char *color[], Course *pred[], Course *black[], int
 int main() {
     char buffer[256];
     Course *courses[MAX_COURSES];
-    char *color[MAX_COURSES];
+    Course *color[MAX_COURSES];
     Course *pred[MAX_COURSES];
     Course *black[MAX_COURSES];
     int num_black = 0;
@@ -78,31 +78,31 @@ int main() {
         courses[num_courses++] = course;
     }
 
-        fclose(fp);
+    fclose(fp);
 
     for (int i = 0; i < num_courses; i++) {
-        Course *course = courses[i];
-        for (int j = 0; j < course->num_prereqs; j++) {
-            char *name = course->prereqs[j]->name;
-            for (int k = 0; k < num_courses; k++) {
-                if (strcmp(courses[k]->name, name) == 0) {
-                    courses[k]->prereqs[courses[k]->num_prereqs++] = course;
-                    break;
-                }
+    Course *course = courses[i];
+    for (int j = 0; j < course->num_prereqs; j++) {
+        char *name = course->prereqs[j]->name;
+        for (int k = 0; k < num_courses; k++) {
+            if (strcmp(courses[k]->name, name) == 0) {
+                courses[k]->prereqs[courses[k]->num_prereqs++] = course;
+                break;
             }
         }
     }
+}
 
-    DFS(courses, num_courses, color, pred, black, &num_black);
+DFS(courses, num_courses, color, pred, black, &num_black);
 
-    printf("Topological order:\n");
-    for (int i = num_black - 1; i >= 0; i--) {
-        printf("%s\n", black[i]->name);
-    }
+printf("Topological order:\n");
+for (int i = num_black - 1; i >= 0; i--) {
+    printf("%s\n", black[i]->name);
+}
 
-    for (int i = 0; i < num_courses; i++) {
-        free(courses[i]);
-    }
+for (int i = 0; i < num_courses; i++) {
+    free(courses[i]);
+}
 
-    return EXIT_SUCCESS;
+return EXIT_SUCCESS;
 }
