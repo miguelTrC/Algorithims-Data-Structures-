9db4d27a45cbadd81c0c356cf3d22848e07dac78
miguelTrC <111913143+miguelTrC@users.noreply.github.com>
2023-05-02 20:31:01 -0500

    Add files via upload

--- HW7/Unix/adjusments.c
+++ HW7/Unix/adjusments.c
@@ -1,16 +1,35 @@
 
 I added the following lines after the while loop that reads from the file:
-(AFTER fclose())
+
 ```c
-printf("\nCourses from file and its pre-reqs:\n");
-printf("Vertex\tCourse\n");
+// Printing vertex and course mapping
+printf("\nVertex\tCourse\n");
 for(int x = 0; x < courseCount; x++){
-    printf("%d\t%s: ", x, courses[x].name);
-    for(int y = 0; y < courses[x].prereqCount; y++){
-        printf("%s", courses[x].prerequisites[y].name);
-        if(y < courses[x].prereqCount-1){
-            printf(", ");
+    printf("%d\t%s\n", x, courses[x].name);
+} // after pre-req(if any)
+
+// Printing adjacency matrix
+printf("\nEdge Information as Adjacency Matrix:\n");
+printf("   ");
+for(int x = 0; x < courseCount; x++){
+    printf("%s ", courses[x].name);
+}
+printf("\n");
+for(int x = 0; x < courseCount; x++){
+    printf("%s ", courses[x].name);
+    for(int y = 0; y < courseCount; y++){
+        int found = 0;
+        for(int z = 0; z < courses[y].prereqCount; z++){
+            if(strcmp(courses[x].name, courses[y].prerequisites[z].name) == 0){
+                printf("1 ");
+                found = 1;
+                break;
+            }
+        }
+        if(found == 0){
+            printf("0 ");
         }
     }
     printf("\n");
 }
+``
